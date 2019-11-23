92ed545c2973dc7cf33d1509b9cf0e2078de294f
Bryan Wyatt <brwyatt@gmail.com>
2019-11-23 09:19:01 -0800

    Add base package skeleton

---
+++ setup.py
@@ -0,0 +1,23 @@
+import os
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='ophiuchus',
+    version='0.1.0',
+    author='Bryan Wyatt',
+    author_email='brwyatt@gmail.com',
+    description=(''),
+    license='LGPLv3',
+    keywords='aws web website serverless lambda',
+    url='https://github.com/brwyatt/Ophiuchus',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    package_data={},
+    include_package_data=False,
+    entry_points={},
+    install_requires=[
+        'boto3==1.10.26',
+    ]
+)
