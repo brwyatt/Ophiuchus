fbd9db9894c8819cc0b09fb5fbb95e5352060245
Bryan Wyatt <brwyatt@gmail.com>
2019-11-23 10:18:20 -0800

    Add and run formatters ad pre-commit hooks
    Enforce and ensure style compliance. Hopefully should reduce conflicts
    and future "format fix" commits.

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Ophiuchus
 Library to create, build, and deploy serverless website stacks on AWS
+
+## Development
+
+### Setup
+
+1. Create Python Virtual Environment:  
+   `python3 -m venv env`
+2. Activate Python venv:  
+   `source env/bin/activate`
+3. Install dev tools:  
+   `pip3 install -r requirements-dev.txt`
+4. Install pre-commit hooks:  
+   `pre-commit install`
+5. Install package for development:  
+   `pip3 install -e .`
+
+### Development
+
+Edit files as usual.
+
+If the pre-commit hooks are installed, formatting and style will be checked and applied before committing. If not installed, run formatters manually prior to submitting a Pull Request to ensure consistency across the project.
--- setup.py
+++ setup.py
@@ -1,24 +1,21 @@
-import os
-
-from setuptools import setup, find_packages
+from setuptools import find_packages
+from setuptools import setup
 
 
 setup(
-    name='ophiuchus',
-    version='0.1.0',
-    author='Bryan Wyatt',
-    author_email='brwyatt@gmail.com',
-    description=(''),
-    license='LGPLv3',
-    keywords='aws web website serverless lambda',
-    url='https://github.com/brwyatt/Ophiuchus',
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
+    name="ophiuchus",
+    version="0.1.0",
+    author="Bryan Wyatt",
+    author_email="brwyatt@gmail.com",
+    description=(""),
+    license="LGPLv3",
+    keywords="aws web website serverless lambda",
+    url="https://github.com/brwyatt/Ophiuchus",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     package_data={},
-    python_requires='~=3.6',
+    python_requires="~=3.6",
     include_package_data=False,
     entry_points={},
-    install_requires=[
-        'boto3==1.10.26',
-    ]
+    install_requires=["boto3==1.10.26"],
 )
