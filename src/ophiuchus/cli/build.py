7032963f0a92e9599312451b32bb8e7aaf8012c2
Bryan Wyatt <brwyatt@gmail.com>
2019-11-23 13:26:29 -0800

    Add a stub for the build command's init
    ... mainly to make sure I remember type hints later...

--- src/ophiuchus/cli/build.py
+++ src/ophiuchus/cli/build.py
@@ -1,5 +1,10 @@
+from argparse import ArgumentParser
+
 from ophiuchus.cli.subcommands import Subcommand
 
 
 class Build(Subcommand):
     description = "Build website"
+
+    def __init__(self, parser: ArgumentParser):
+        super().__init__(parser)
