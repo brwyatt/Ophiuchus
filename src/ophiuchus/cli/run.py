290718b2f5f7e9a5a973d7b985f19f3cdbdc1fde
Bryan Wyatt <brwyatt@gmail.com>
2019-11-23 13:25:48 -0800

    Add a little bit of something for the run command
    Not much, but at least it is starting to do something...

--- src/ophiuchus/cli/run.py
+++ src/ophiuchus/cli/run.py
@@ -1,5 +1,21 @@
+from argparse import ArgumentParser
+
 from ophiuchus.cli.subcommands import Subcommand
+from ophiuchus.utils import load_entry_points
 
 
 class Run(Subcommand):
     description = "Run website locally"
+
+    def __init__(self, parser: ArgumentParser):
+        super().__init__(parser)
+
+        parser.add_argument(
+            "site_name",
+            type=str,
+            help="Entrypoint group name for website Lambda handlers",
+        )
+
+    def __call__(self, site_name, *args, **kwargs) -> int:
+        handlers = load_entry_points(site_name)
+        self.log.debug(handlers)
