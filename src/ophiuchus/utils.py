6b9bff9327feca0e6d74c6e253b54221650a1ff4
Bryan Wyatt <brwyatt@gmail.com>
2019-11-23 13:25:06 -0800

    Add more logging to load_entry_points

--- src/ophiuchus/utils.py
+++ src/ophiuchus/utils.py
@@ -1,7 +1,11 @@
+import logging
 from typing import Dict
 from typing import Optional
 
 import pkg_resources
+
+
+log = logging.getLogger(__name__)
 
 
 def load_entry_points(
@@ -9,14 +13,32 @@ def load_entry_points(
 ) -> Dict[str, callable]:
     entry_points = {}
 
+    log.info(f'Loading entry points for "{group}"')
+
     for entry_point in pkg_resources.iter_entry_points(group):
-        loaded = entry_point.load()
+        log.debug(f'Loading entry point "{entry_point.name}" from "{group}"')
+
+        try:
+            loaded = entry_point.load()
+        except Exception as e:
+            msg = (
+                f'Failed to load Entry point "{entry_point.name}" from '
+                f'"{group}": {e}'
+            )
+            log.error(msg)
+            raise e
+
         if type_constraint and not issubclass(loaded, type_constraint):
-            raise TypeError(
+            msg = (
                 f'Entry Point "{entry_point.name}" from "{group}" does not '
                 f'match type constraint for "{type_constraint.__module__}.'
                 f'{type_constraint.__name__}".',
             )
+            log.error(msg)
+            raise TypeError(msg)
+
+        log.debug(f'Successfully loaded "{entry_point.name}" from "{group}"')
         entry_points[entry_point.name] = loaded
 
+    log.debug(f'Finished loading {len(entry_points)} from "{group}"')
     return entry_points
